bb7b7bb7b8349c68c2645cac5af2ec6a0d762915
tunji-solomon <tunjisolomon10000@gmail.com>
2024-01-20 12:04:47 +0100

    updated settings.py

--- students_app/settings.py
+++ students_app/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
