df0d33c55071f8ec70f1be09aadfc6f92385246f
tunji-solomon <tunjisolomon10000@gmail.com>
2024-01-21 00:56:57 +0100

    modified settings.py

--- students_app/settings.py
+++ students_app/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
@@ -143,5 +143,3 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'easyloan100@outlook.com'
 EMAIL_HOST_PASSWORD = 'AllGod100'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
