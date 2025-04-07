909695f50ea0d6b55f3eb2236581617e078de14b
tunji-solomon <tunjisolomon10000@gmail.com>
2025-04-07 17:41:25 +0100

    Database change and migrations

--- students_app/settings.py
+++ students_app/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('thisisatestsecret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
